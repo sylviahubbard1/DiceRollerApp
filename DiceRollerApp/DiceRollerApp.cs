86e37507ced4ce51853396aca4e088c14d8de111
sylviahubbard1 <sylviahubbard@gmail.com>
2016-10-19 11:09:11 -0400

    DiceRolling

---
+++ DiceRollerApp/DiceRollerApp.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollerApp
+{
+    public class DiceRoll
+    {
+        public void messageDice(int d1, int d2)
+        {
+            if (d1 == 6 && d2 == 6)
+
+            {
+                Console.WriteLine("Box Cars");
+            }
+            else if (d1 == 1 && d2 == 1)
+            {
+                Console.WriteLine("Snake Eyes");
+            }
+            else if (d1 == 2 && d2 == 3)
+            {
+                Console.WriteLine("CRAPS");
+            }
+            else if (d2 == 2 && d1 == 3)
+            {
+                Console.WriteLine("CRAPS");
+            }
+
+        }
+    }
+}
--- DiceRollerApp/Program.cs
+++ DiceRollerApp/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DiceRollerApp;
 
-namespace DiceRollerApp
+namespace DiceRolConsoleApplication_LabFive_31AuglerApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+                    
+            Console.WriteLine(string.Format("Welcome to the Grand Circus Casino!"));
+            Random rnd = new Random();
+            Console.WriteLine("Roll the dice?(y/n)");
+            string reply = Console.ReadLine();
+            int counter = 1;
+            int numberOfSide;
+
+            while (reply.Trim().ToLower() == "y")
+            {
+                Console.WriteLine("Enter the number of sides for a pair of dice ?");
+                bool side = int.TryParse(Console.ReadLine(), out numberOfSide);
+                if (side == true)
+                {
+                    Console.WriteLine($"Roll : {counter}");
+                    int dice1 = rnd.Next(1, numberOfSide);
+                    int dice2 = rnd.Next(1, numberOfSide);
+                    Console.WriteLine($"{dice1} \n{dice2}");
+                    // Console.WriteLine(string.Format("{0}\n{1}", dice1, dice2));                  
+                    // Console.WriteLine((dice2));                    
+                    DiceRoll roll = new DiceRoll();
+                    roll.messageDice(dice1, dice2);
+                    counter++;
+                    Console.WriteLine("Roll again?(y/n)");
+                    reply = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Enter an interger");
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("\nAre you sure you don't want to play?");
         }
     }
 }
